9d100cb996f1b3edaf7e508d329b69991482f1b4
Andreas Moik <moik@technikum-wien.at>
2016-06-23 16:44:12 +0200

    added curl

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,13 +1,5 @@
-
-# Uncomment this if you're using STL in your project
-# See CPLUSPLUS-SUPPORT.html in the NDK documentation for more information
-# APP_STL := stlport_static
-
 APP_ABI := armeabi armeabi-v7a x86
 APP_STL := stlport_static
 APP_PIE := true
-APP_PLATFORM := android-16
+APP_PLATFORM := android-15
 
-#old shit! TODO
-#APP_ABI := armeabi armeabi-v7a x86
-#APP_STL:=stlport_static / c++_shared 
