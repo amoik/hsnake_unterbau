e6db15c1f42f6a896a23640a3ca7de142b57e8f8
Andreas Moik <moik@technikum-wien.at>
2016-08-03 10:55:59 +0200

    transition to gnustl new .gitignore entries

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,5 +1,4 @@
-APP_ABI := armeabi #x86_64 armeabi armeabi-v7a x86
-APP_STL := stlport_static
+APP_ABI := armeabi #x86_64 armeabi armeabi-v7a x86 all
+APP_STL := gnustl_static
 APP_PIE := true
 APP_PLATFORM := android-15
-
