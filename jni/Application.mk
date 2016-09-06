a49c381bfbaae9abfba221e772730a09d65a49ee
Andreas Moik <moik@technikum-wien.at>
2016-09-06 18:33:39 +0200

    added ssl and crypto

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,4 +1,4 @@
-APP_ABI := armeabi x86 mips #x86_64 armeabi armeabi-v7a x86 all
+APP_ABI := armeabi #x86_64 armeabi armeabi-v7a x86 all
 APP_STL := gnustl_static
 APP_PIE := true
 APP_PLATFORM := android-15
--- jni/ssl/Android.mk
+++ jni/ssl/Android.mk
@@ -1,9 +1,8 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := curl
+LOCAL_MODULE := libssl
+LOCAL_SRC_FILES := prec/$(TARGET_ARCH_ABI)/libssl.a
 LOCAL_C_INCLUDES += include
-LOCAL_SRC_FILES := prec/$(TARGET_ARCH_ABI)/libcurl.a
-LOCAL_STATIC_LIBRARIES := ssl crypto
 
 include $(PREBUILT_STATIC_LIBRARY)
