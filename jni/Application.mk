9570326b357340b18bfc5e7e1e88caef88123088
Andreas Moik <moik@technikum-wien.at>
2016-09-07 09:15:21 +0200

    enabled x86 and mips

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,4 +1,4 @@
-APP_ABI := armeabi #x86_64 armeabi armeabi-v7a x86 all
+APP_ABI := x86 mips armeabi #x86_64 armeabi armeabi-v7a x86 all
 APP_STL := gnustl_static
 APP_PIE := true
 APP_PLATFORM := android-15
